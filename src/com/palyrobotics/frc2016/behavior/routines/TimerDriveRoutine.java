9b08f3da608ab4f1654c20bd16fd0f2278d2c74b
CalvinYan <calviny524@gmail.com>
2016-09-27 21:32:13 -0700

    Slightly modified timer routine

--- src/com/palyrobotics/frc2016/behavior/routines/TimerDriveRoutine.java
+++ src/com/palyrobotics/frc2016/behavior/routines/TimerDriveRoutine.java
@@ -13,16 +13,36 @@ import edu.wpi.first.wpilibj.Timer;
 public class TimerDriveRoutine extends Routine {
 
 	public enum TimerDriveRoutineStates {
-		START, DRIVING, DONE, IDLE
+		START, DRIVING, DONE
 	}
 
-	TimerDriveRoutineStates m_state = TimerDriveRoutineStates.IDLE;
+	TimerDriveRoutineStates m_state = TimerDriveRoutineStates.START;
 	Timer m_timer = new Timer();
 	// Default values for time and velocity setpoints
 	private double m_time_setpoint = 3;
 	private double m_velocity_setpoint = 0.5;
+	
+	private boolean m_is_new_state = true;
 
 	private Drive drive = HardwareAdaptor.kDrive;
+	
+	/**
+	 * Constructs with a specified time setpoint
+	 * @param time How long to drive (seconds)
+	 */
+	public TimerDriveRoutine(double time) {
+		this.m_time_setpoint = time;
+	}
+	
+	/**
+	 * Constructs with a specified time setpoint and velocity
+	 * @param time How long to drive (seconds)
+	 * @param velocity What velocity to drive at (0 to 1)
+	 */
+	public TimerDriveRoutine(double time, double velocity) {
+		this.m_time_setpoint = time;
+		setVelocity(velocity);
+	}
 	
 	/**
 	 * Sets the time setpoint that will be used
@@ -51,53 +71,58 @@ public class TimerDriveRoutine extends Routine {
 		RobotSetpoints setpoints = existing_setpoints;
 		switch (m_state) {
 		case START:
-			setpoints.timer_drive_time_setpoint = Optional.of(m_time_setpoint);
-			setpoints.drive_routine_action = RobotSetpoints.DriveRoutineAction.TIMER_DRIVE;
-			setpoints.drive_velocity_setpoint = Optional.of(m_velocity_setpoint);
 			m_timer.reset();
 			m_timer.start();
+			// Only set the setpoint the first time the state is START 
+			if(m_is_new_state) {
+				setpoints.timer_drive_time_setpoint = Optional.of(m_time_setpoint);
+				setpoints.drive_velocity_setpoint = Optional.of(m_velocity_setpoint);
+			}
+
+			setpoints.drive_routine_action = RobotSetpoints.DriveRoutineAction.TIMER_DRIVE;
 			new_state = TimerDriveRoutineStates.DRIVING;
 			break;
 		case DRIVING:
+			setpoints.timer_drive_time_setpoint = Optional.of(m_time_setpoint);
+			setpoints.drive_velocity_setpoint = Optional.of(m_velocity_setpoint);
 			if(m_timer.get() > m_time_setpoint) {
-				setpoints.timer_drive_time_setpoint = RobotSetpoints.m_nullopt;
 				new_state = TimerDriveRoutineStates.DONE;
 			}
 			break;
 		case DONE:
-			setpoints.timer_drive_time_setpoint = RobotSetpoints.m_nullopt;
-			setpoints.drive_velocity_setpoint = RobotSetpoints.m_nullopt;
 			drive.reset();
 			setpoints.drive_routine_action = RobotSetpoints.DriveRoutineAction.NONE;
-			
-			new_state = TimerDriveRoutineStates.IDLE;
-			break;
-		case IDLE:
-			// Do nothing, thus not interfering with other routines
 			break;
 		}
-		m_state = new_state;
-		return setpoints;
-	}
 
-	@Override
-	public void start() {
-		m_state = TimerDriveRoutineStates.DONE;
-		m_timer.stop();
-		m_timer.reset();
+		m_is_new_state = false;
+		if(new_state != m_state) {
+			m_state = new_state;
+			m_timer.reset();
+			m_is_new_state = true;
+		}
+		
+		return setpoints;
 	}
 
 	@Override
 	public void cancel() {
 		m_state = TimerDriveRoutineStates.DONE;
 		m_timer.stop();
-		drive.setOpenLoop(new DriveSignal(0, 0));
+		m_timer.reset();
+		drive.setOpenLoop(DriveSignal.NEUTRAL);
 		drive.reset();
 	}
-
+	
+	@Override
+	public void start() {
+		drive.reset();
+		m_timer.reset();
+	}
+	
 	@Override
 	public boolean isFinished() {
-		return m_state == TimerDriveRoutineStates.DONE || m_state == TimerDriveRoutineStates.IDLE;
+		return m_state == TimerDriveRoutineStates.DONE;
 	}
 
 	@Override
