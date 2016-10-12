a4d00144708edbc655281434ce86cd0088554810
Eric Liu <ericxliu@gmail.com>
2016-10-12 15:07:54 -0700

    added auto align action

--- src/com/palyrobotics/frc2016/auto/actions/AutoAlignAction.java
+++ src/com/palyrobotics/frc2016/auto/actions/AutoAlignAction.java
@@ -1,5 +1,11 @@
 package com.palyrobotics.frc2016.auto.actions;
 
+import com.palyrobotics.frc2016.HardwareAdaptor;
+import com.palyrobotics.frc2016.subsystems.Drive;
+import com.team254.lib.util.DriveSignal;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
 /**
  * Aligns to the goal
  * TODO: Everything
@@ -8,21 +14,32 @@ package com.palyrobotics.frc2016.auto.actions;
  */
 public class AutoAlignAction implements Action {
 
+	
+	private NetworkTable table = NetworkTable.getTable("visiondata");
+	private Drive drive = HardwareAdaptor.kDrive;
+	double skewAngle = table.getNumber("skewangle", 10000);
+	
 	@Override
 	public boolean isFinished() {
-		// TODO Auto-generated method stub
-		return true;
+		if(Math.abs(skewAngle) < 3 || skewAngle == 10000) {
+			return true;
+		} 
+		else return false;
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-
+		skewAngle = table.getNumber("skewangle", 10000);
+		skewAngle = (skewAngle >=0) ? skewAngle-2:skewAngle+2;
+		if(!drive.hasController()) {
+			drive.setAutoAlignSetpoint(skewAngle);
+		}
 	}
 
 	@Override
 	public void done() {
 		System.out.println("Finished auto align");
+		drive.setOpenLoop(DriveSignal.NEUTRAL);
 	}
 
 	@Override
